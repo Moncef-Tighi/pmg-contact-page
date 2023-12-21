315eca9d8f048e51a8125fedbea174cde226710a
Moncef-Tighi <monceftighiouart@gmail.com>
2023-12-21 12:02:23 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      custom1: ["BebasNeue"],
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['"BebasNeue"', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    },
+  },
+}
