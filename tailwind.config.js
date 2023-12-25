353af78f1d61738803617db45bf34eb789102630
Moncef-Tighi <monceftighiouart@gmail.com>
2023-12-25 15:25:56 +0100

    Ajout de toute les images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      custom1: ["BebasNeue"],
       cursive: ["cursive"],
     },
     extend: {
       fontFamily: {
-        'sans': ['"BebasNeue"', 'Helvetica', 'Arial', 'sans-serif']
+        'sans': ['Helvetica', 'Arial', 'sans-serif']
       },
       animation: {
         'infinite-scroll': 'infinite-scroll 24s linear infinite',
